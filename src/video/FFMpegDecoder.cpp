pararthshah/libavg-vaapi

commit cdba67c3e1ed0dc6f0be48023400c21fde81c7ea

Added error message when trying to play back audio with an unsupported sample format (Bug #239).
git-svn-id: https://www.libavg.de/svn/trunk/libavg@6390 44470bb9-56e9-0310-a0f8-c586564d3dc6

src/video/FFMpegDecoder.cpp
@@ -255,6 +255,11 @@ void FFMpegDecoder::open(const string& sFilename, bool bThreadedDemuxer)
             AVG_TRACE(Logger::WARNING, 
                     sFilename + ": unsupported codec ("+szBuf+"). Disabling audio.");
         }
+        if (m_pAStream->codec->sample_fmt != SAMPLE_FMT_S16) {
+            m_AStreamIndex = -1;
+            AVG_TRACE(Logger::WARNING, 
+                    sFilename + ": unsupported sample format (!= S16). Disabling audio.");
+        }
     }
 
     m_State = OPENED;

commit 4878845afef31f0f710958a6a511460e23d112a6

Fixed crash when playing back videos with surround audio (introduced in r6390).
git-svn-id: https://www.libavg.de/svn/trunk/libavg@6410 44470bb9-56e9-0310-a0f8-c586564d3dc6

src/video/FFMpegDecoder.cpp
@@ -257,6 +257,7 @@ void FFMpegDecoder::open(const string& sFilename, bool bThreadedDemuxer)
         }
         if (m_pAStream->codec->sample_fmt != SAMPLE_FMT_S16) {
             m_AStreamIndex = -1;
+            m_pAStream = 0; 
             AVG_TRACE(Logger::WARNING, 
                     sFilename + ": unsupported sample format (!= S16). Disabling audio.");
         }