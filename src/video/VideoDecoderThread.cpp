pararthshah/libavg-vaapi

commit d33011fdb49864b647304a93e432390155d0a0cd

Fixed decode of last frames before eof in some videos.
git-svn-id: https://www.libavg.de/svn/trunk/libavg@8763 44470bb9-56e9-0310-a0f8-c586564d3dc6

src/video/VideoDecoderThread.cpp
@@ -117,9 +117,12 @@ void VideoDecoderThread::decodePacket(AVPacket* pPacket)
 void VideoDecoderThread::handleEOF()
 {
     AVFrame frame;
-    bool bGotPicture = m_pFrameDecoder->decodeLastFrame(frame);
-    if (bGotPicture) {
-        sendFrame(frame);
+    bool bGotPicture = true;
+    while (bGotPicture) {
+        bGotPicture = m_pFrameDecoder->decodeLastFrame(frame);
+        if (bGotPicture) {
+            sendFrame(frame);
+        }
     }
     VideoMsgPtr pMsg(new VideoMsg());
     pMsg->setEOF();

src/video/VideoDemuxerThread.cpp
@@ -130,7 +130,6 @@ void VideoDemuxerThread::close()
 void VideoDemuxerThread::onStreamEOF(int streamIndex)
 {
     m_PacketQEOFMap[streamIndex] = true;
-                
     m_bEOF = true;
     map<int, bool>::iterator it;
     for (it = m_PacketQEOFMap.begin(); it != m_PacketQEOFMap.end(); it++) {

src/video/testvideo.cpp
@@ -458,10 +458,8 @@ class AVDecoderTest: public DecoderTest {
                 curTime += 1.0f/pDecoder->getFPS();
             }
             TEST(pDecoder->isEOF());
-            // TODO: Commented out because the last frames aren't decoded with newer
-            // ffmpeg (circa >= 0.8)
-            // TEST(numFrames == expectedNumFrames);
-            // testEqual(*pBmp, sFilename+"_end", B8G8R8X8);
+            TEST(numFrames == expectedNumFrames);
+            testEqual(*pBmp, sFilename+"_end", B8G8R8X8);
 
             if (isThreaded()) {
                 // Check if audio length was ok.

commit 31f32794b2967816123240426ac77a063f4e1d1e

Fixed video hang on close() during EOF processing (introduced in r8763).
git-svn-id: https://www.libavg.de/svn/trunk/libavg@8786 44470bb9-56e9-0310-a0f8-c586564d3dc6

src/video/VideoDecoderThread.cpp
@@ -46,7 +46,8 @@ VideoDecoderThread::VideoDecoderThread(CQueue& cmdQ, VideoMsgQueue& msgQ,
       m_Size(size),
       m_PF(pf),
       m_bUseVDPAU(bUseVDPAU),
-      m_bSeekDone(false)
+      m_bSeekDone(false),
+      m_bProcessingLastFrames(false)
 {
     m_pFrameDecoder = FFMpegFrameDecoderPtr(new FFMpegFrameDecoder(pStream));
 }
@@ -61,27 +62,34 @@ static ProfilingZoneID PacketWaitProfilingZone("Video wait for packet", true);
 bool VideoDecoderThread::work() 
 {
     ScopeTimer timer(DecoderProfilingZone);
-    VideoMsgPtr pMsg;
-    {
-        ScopeTimer timer(PacketWaitProfilingZone);
-        pMsg = m_PacketQ.pop(true);
-    }
-    switch (pMsg->getType()) {
-        case VideoMsg::PACKET:
-            decodePacket(pMsg->getPacket());
-            break;
-        case VideoMsg::END_OF_FILE:
-            handleEOF();
-            break;
-        case VideoMsg::SEEK_DONE:
-            handleSeekDone(pMsg);
-            break;
-        case VideoMsg::CLOSED:
-            close();
-            break;
-        default:
-            pMsg->dump();
-            AVG_ASSERT(false);
+    if (m_bProcessingLastFrames) {
+        // EOF received, but last frames still need to be decoded.
+        handleEOF();
+    } else {
+        // Standard decoding.
+        VideoMsgPtr pMsg;
+        {
+            ScopeTimer timer(PacketWaitProfilingZone);
+            pMsg = m_PacketQ.pop(true);
+        }
+        switch (pMsg->getType()) {
+            case VideoMsg::PACKET:
+                decodePacket(pMsg->getPacket());
+                break;
+            case VideoMsg::END_OF_FILE:
+                handleEOF();
+                m_bProcessingLastFrames = true;
+                break;
+            case VideoMsg::SEEK_DONE:
+                handleSeekDone(pMsg);
+                break;
+            case VideoMsg::CLOSED:
+                close();
+                break;
+            default:
+                pMsg->dump();
+                AVG_ASSERT(false);
+        }
     }
     ThreadProfiler::get()->reset();
     return true;
@@ -117,16 +125,15 @@ void VideoDecoderThread::decodePacket(AVPacket* pPacket)
 void VideoDecoderThread::handleEOF()
 {
     AVFrame frame;
-    bool bGotPicture = true;
-    while (bGotPicture) {
-        bGotPicture = m_pFrameDecoder->decodeLastFrame(frame);
-        if (bGotPicture) {
-            sendFrame(frame);
-        }
+    bool bGotPicture = m_pFrameDecoder->decodeLastFrame(frame);
+    if (bGotPicture) {
+        sendFrame(frame);
+    } else {
+        m_bProcessingLastFrames = false;
+        VideoMsgPtr pMsg(new VideoMsg());
+        pMsg->setEOF();
+        pushMsg(pMsg);
     }
-    VideoMsgPtr pMsg(new VideoMsg());
-    pMsg->setEOF();
-    pushMsg(pMsg);
 }
 
 void VideoDecoderThread::handleSeekDone(VideoMsgPtr pMsg)

src/video/VideoDecoderThread.h
@@ -72,6 +72,7 @@ class AVG_API VideoDecoderThread: public WorkerThread<VideoDecoderThread> {
         bool m_bUseVDPAU;
 
         bool m_bSeekDone;
+        bool m_bProcessingLastFrames;
 };
 
 }